4f8471d97eb73e6858f408ffdad8b06fd5775acd
puna47 <puna47@gmail.com>
2016-12-01 14:16:26 +0100

    Initializing clients variables

--- pcss_client_code/pcss_client_code/Program.cs
+++ pcss_client_code/pcss_client_code/Program.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
 
 namespace pcss_client_code
 {
-    class Program
+    class MT_Client
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.WriteLine("Starting echo client...");
+            
+            int port = 1234;
+            TcpClient client = new TcpClient("localhost", port);
+            NetworkStream stream = client.GetStream();
+            StreamReader reader = new StreamReader(stream);
+            StreamWriter writer = new StreamWriter(stream) { AutoFlush = true };
         }
     }
 }
