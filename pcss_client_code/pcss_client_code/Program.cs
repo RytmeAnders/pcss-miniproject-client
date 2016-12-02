feb122b382738da729bcb03b75eed95931887372
sweetamber <rasmus_johannsen@hotmail.com>
2016-12-02 21:01:03 +0100

    fixes #4

--- pcss_client_code/pcss_client_code/Program.cs
+++ pcss_client_code/pcss_client_code/Program.cs
@@ -13,19 +13,20 @@ namespace pcss_client_code
     {
         static void Main()
         {
-            Console.WriteLine("Starting echo client...");
-
+            //initializes script variables         
             int playerNumber = 0;
             int mySecretNumber = 0;
             bool lobby = false;
             int port = 0;
             string IP = "";
 
+           //request specific ip and port of the server
             Console.WriteLine("Please Enter the IP of the server");
             IP = Console.ReadLine();
             Console.WriteLine("Please enter the port of the server");
             port = Int32.Parse(Console.ReadLine());
 
+            //Creates the tcpClient 
             TcpClient client = new TcpClient(IP, port);
             NetworkStream stream = client.GetStream();
             StreamReader reader = new StreamReader(stream);
@@ -43,6 +44,7 @@ namespace pcss_client_code
             lobby = true;
             Console.Clear();
 
+            //The while loop initializes the lobby 
             while (lobby)
             {
                 string LobbyResponse = reader.ReadLine();
@@ -54,13 +56,14 @@ namespace pcss_client_code
                     Console.WriteLine("player " + playerNumber + ":");
                     LobbyResponse = "";
                 }
+                //Updates the current number of players in the lobby for every
                 if (LobbyResponse.Equals("update"))
                 {
                     string message = reader.ReadLine();
                     Console.WriteLine(message);
                     LobbyResponse = "";
                 }
-
+                //Sends message from the server whenever the lobby is full 
                 if (LobbyResponse.Equals("Game Started"))
                 {
                     lobby = false;
@@ -68,19 +71,22 @@ namespace pcss_client_code
                 }
             }
 
-
+            //Goes out of the lobby and starts the game
             if (!lobby)
             {
                 Console.Clear();
+                //Keeps the game running
                 while (true)
                 {
                     // Wait for other players to join / wait to be killed
                     string response = reader.ReadLine();
                     Console.WriteLine(response);
 
+                    //Stops the game from running if another player guesses your 
                     if (response.Equals("Game Over"))
                         break;
 
+                    //Lets you choose a target
                     if (response.Equals("It's your turn!"))
                     {
                         // Target
@@ -89,6 +95,7 @@ namespace pcss_client_code
                         Console.WriteLine();
                     }
 
+                    //Lets you take a guess or change your number after choosing a target
                     if (response.Equals("Now guess!"))
                     {
                         // Guess
@@ -98,6 +105,7 @@ namespace pcss_client_code
                         Console.WriteLine();
                     }
 
+                    //Used if you choose the change your secret number
                     if (response.Equals("change"))
                     {
                         Console.WriteLine("Enter your new secret number:");
