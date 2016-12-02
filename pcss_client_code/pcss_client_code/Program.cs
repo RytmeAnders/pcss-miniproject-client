a7e02fc1ea5bab4b49bb078a6b68cae4cb08e4ab
sweetamber <rasmus_johannsen@hotmail.com>
2016-12-02 13:03:11 +0100

    Added variables and option to input server ip and port

--- pcss_client_code/pcss_client_code/Program.cs
+++ pcss_client_code/pcss_client_code/Program.cs
@@ -14,12 +14,33 @@ namespace pcss_client_code
         static void Main()
         {
             Console.WriteLine("Starting echo client...");
-            
+
+            int playerNumber = 0;
+            int mySecretNumber = 0;
+            bool lobby = false;
             int port = 1234;
+            string IP = "127.0.0.1";
+
+            Console.WriteLine("Please Enter the IP of the server");
+            IP = Console.ReadLine();
+            Console.WriteLine("Please enter the port of the server");
+            port = Int32.Parse(Console.ReadLine());
+
             TcpClient client = new TcpClient("localhost", port);
             NetworkStream stream = client.GetStream();
             StreamReader reader = new StreamReader(stream);
             StreamWriter writer = new StreamWriter(stream) { AutoFlush = true };
+
+            //confirms the player is connected
+            Console.WriteLine("You are connected");
+
+            // Choose your number 0-9
+            Console.WriteLine("Enter your secret number:");
+            mySecretNumber = Int32.Parse(Console.ReadLine());
+
+            // Let the server know our number
+            writer.WriteLine(mySecretNumber);
+
         }
     }
 }
